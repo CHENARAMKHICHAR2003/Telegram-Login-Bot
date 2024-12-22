ae133da144ac3e025331cca7e1f68e5b75a9684c
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2024-12-23 01:18:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+# Telegram API credentials
+API_ID = ''  # Replace with your actual API ID
+API_HASH = ''  # Replace with your actual API hash
+BOT_TOKEN = ''  # Replace with your bot's token
