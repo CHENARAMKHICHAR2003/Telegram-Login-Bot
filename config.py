af7b5551f41b132a3eac2001213ba777f46054d6
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2024-12-23 02:09:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-# config.py
-
-# Telegram API credentials
-API_ID = '24894984'   
-API_HASH = '4956e23833905463efb588eb806f9804' 
-BOT_TOKEN = ''
+# API credentials for Telegram Bot and Pyrogram Client
+API_ID = '24894984'  # Replace with your actual API ID from Telegram
+API_HASH = '4956e23833905463efb588eb806f9804'  # Replace with your actual API HASH from Telegram
+BOT_TOKEN = ''  # Replace with your bot's token (available from BotFather)
+CHANNEL_ID = '@jaathub56'  # Replace with your channel's username or ID
